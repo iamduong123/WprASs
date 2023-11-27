bc6a1aec4dcca72e7543be7909b21544d13ac6e0
iamduong123 <htduong.22003@gmail.com>
2023-11-27 10:29:34 +0700

    modify user interface and update database table

--- dbsetup.js
+++ dbsetup.js
@@ -44,52 +44,76 @@ function createTables() {
     body TEXT,
     attachment_path VARCHAR(255),
     time_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_deleted BOOLEAN DEFAULT 0,
     FOREIGN KEY (sender_id) REFERENCES users(id),
     FOREIGN KEY (recipient_id) REFERENCES users(id) 
   );`;
+  const createDeletedEmailsTable = `CREATE TABLE IF NOT EXISTS deleted_emails (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email_id INT,
+    user_id INT,
+    FOREIGN KEY (email_id) REFERENCES emails(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+  );`;
+  
   // SQL query to insert users data
   const insertUsers = `
-  INSERT INTO users (full_name, email, password) VALUES
-    ('User1', 'a@a.com', 'password1'),
-    ('User2', 'b@b.com', 'password2'),
-    ('User3', 'c@c.com', 'password3');
+INSERT INTO users (full_name, email, password) VALUES
+  ('User1', 'a@a.com', 'password1'),
+  ('User2', 'b@b.com', 'password2'),
+  ('User3', 'c@c.com', 'password3');
 `;
 
 
     // SQL query to initialize data for users
     const insertEmails = `INSERT INTO emails (sender_id, recipient_id, subject, body) VALUES
-    (1, 2, 'Hello', 'Hi User2, how are you?'),
-    (2, 1, 'Meeting', 'Are you available for a meeting tomorrow?'),
-    (3, 1, 'Regarding Project', 'Let''s discuss the project details.'),
-    (1, 3, 'Re: Regarding Project', 'Sure, let''s schedule a meeting.'),
-    (2, 1, 'Quick Question', 'Do you have the latest updates?'),
-    (1, 2, 'Re: Quick Question', 'Yes, I''ll share them with you.'),
-    (3, 2, 'Status Update', 'What''s the status of the task?'),
-    (2, 3, 'Re: Status Update', 'Everything is on track.');`;
+  (1, 2, 'Hello', 'Hi User2, how are you?'),
+  (2, 1, 'Meeting', 'Are you available for a meeting tomorrow?'),
+  (3, 1, 'Regarding Project', 'Let''s discuss the project details.'),
+  (1, 3, 'Re: Regarding Project', 'Sure, let''s schedule a meeting.'),
+  (2, 1, 'Quick Question', 'Do you have the latest updates?'),
+  (1, 2, 'Re: Quick Question', 'Yes, I''ll share them with you.'),
+  (3, 2, 'Status Update', 'What''s the status of the task?'),
+  (2, 3, 'Re: Status Update', 'Everything is on track.');
+`;
+const addIsDeletedColumnQuery = 'ALTER TABLE emails ADD COLUMN IF NOT EXISTS is_deleted BOOLEAN DEFAULT 0';
+  db.query(addIsDeletedColumnQuery, (alterErr) => {
+    if (alterErr) {
+      console.error('Error adding is_deleted column:', alterErr);
+      return;
+    }
+    console.log('is_deleted column added');
+  });
 
     
   // Execute queries
   db.query(createUsersTable, (err) => {
     if (err) throw err;
     console.log('Users table created');
-
+  
     db.query(createEmailsTable, (err) => {
       if (err) throw err;
       console.log('Emails table created');
-      db.query(insertUsers, (err) => {
+  
+      db.query(createDeletedEmailsTable, (err) => {
         if (err) throw err;
-        console.log('Data user inserted');
-
-        db.query(insertEmails, (err) => {
+        console.log('Deleted emails table created');
+  
+        db.query(insertUsers, (err) => {
           if (err) throw err;
-          console.log('Data email inserted');
-
-        // Close the database connection
-          db.end((err) => {
+          console.log('Data user inserted');
+  
+          db.query(insertEmails, (err) => {
             if (err) throw err;
-            console.log('Database connection closed');
+            console.log('Data email inserted');
+  
+            // Close the database connection
+            db.end((err) => {
+              if (err) throw err;
+              console.log('Database connection closed');
+            });
+          });
         });
       });
     });
-  });
-})};
+  })};
